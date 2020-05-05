51c2b3eda0a713cf059ab6e25cb6751685ef1a07
CoopNich <CoopNich92@gmail.com>
2020-05-05 14:19:51 -0500

    users must login to view libraries, books, and librarians

--- libraryproject/libraryapp/urls.py
+++ libraryproject/libraryapp/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
+from django.urls import include, path
 from .views import *
 
 app_name = "libraryapp"
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('logout/', logout_user, name='logout'),
     path('', home, name='home'),
     path('books/', book_list, name='books'),
     path('librarians/', librarian_list, name='librarians'),
